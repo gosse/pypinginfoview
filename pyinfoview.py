429d0ff324e7a6db49ebb589ab3d62e89dd2a38d
Gary Ossewaarde <8118098+gosse@users.noreply.github.com>
2023-01-25 13:50:17 -0500

    reformatting, refactoring

--- pyinfoview.py
+++ pyinfoview.py
@@ -7,34 +7,53 @@ import time
 import argparse
 
 
-hosts = ['1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4', 'canireachthe.net', '208.67.220.220', '208.67.222.222']
+hosts = [
+    "1.1.1.1",
+    "1.0.0.1",
+    "8.8.8.8",
+    "8.8.4.4",
+    "canireachthe.net",
+    "208.67.220.220",
+    "208.67.222.222",
+]
 
 results = {}
 
+
 def ping_host(host, ping_timeout):
-  if host not in results: results[host] = {}
-  result = ping(host, count=1, privileged=False, timeout=ping_timeout)  
-  results[host]['is_alive'] = result.is_alive
-  if 'history' not in results[host]: results[host]['history'] = []
-  if result.is_alive:
-    results[host]['history'].append(':white_check_mark:')
-  else:
-    results[host]['history'].append(':x:')
-  if 'packets_sent' not in results[host]: results[host]['packets_sent'] = 0
-  results[host]['packets_sent'] += result.packets_sent
-  if 'packets_received' not in results[host]: results[host]['packets_received'] = 0
-  results[host]['packets_received'] += result.packets_received
-  results[host]['packet_loss'] = ((results[host]['packets_sent'] - results[host]['packets_received']) / results[host]['packets_sent']) * 100
-  if 'rtts' not in results[host]: results[host]['rtts'] = []
-  results[host]['rtts'].append(result.avg_rtt)
-  # avg = sum(number_list)/len(number_list)
-  results[host]['last_rtt'] = result.avg_rtt
-  results[host]['average_rtt'] = sum(results[host]['rtts'])/len(results[host]['rtts'])
-  # print(results)
+    if host not in results:
+        results[host] = {}
+    result = ping(host, count=1, privileged=False, timeout=ping_timeout)
+    results[host]["is_alive"] = result.is_alive
+    if "history" not in results[host]:
+        results[host]["history"] = []
+    if result.is_alive:
+        results[host]["history"].append(":white_check_mark:")
+    else:
+        results[host]["history"].append(":x:")
+    if "packets_sent" not in results[host]:
+        results[host]["packets_sent"] = 0
+    results[host]["packets_sent"] += result.packets_sent
+    if "packets_received" not in results[host]:
+        results[host]["packets_received"] = 0
+    results[host]["packets_received"] += result.packets_received
+    results[host]["packet_loss"] = (
+        (results[host]["packets_sent"] - results[host]["packets_received"])
+        / results[host]["packets_sent"]
+    ) * 100
+    if "rtts" not in results[host]:
+        results[host]["rtts"] = []
+    results[host]["rtts"].append(result.avg_rtt)
+    # avg = sum(number_list)/len(number_list)
+    results[host]["last_rtt"] = result.avg_rtt
+    results[host]["average_rtt"] = sum(results[host]["rtts"]) / len(
+        results[host]["rtts"]
+    )
+    # print(results)
 
 
 def generate_table(ping_timeout) -> Table:
-    """Make a new table."""
+    # Make the table
     table = Table()
     table.add_column("Alive")
     table.add_column("Host")
@@ -45,12 +64,12 @@ def generate_table(ping_timeout) -> Table:
     table.add_column("Last RTT")
     table.add_column("Mean RTT")
 
+    # Working loop through the host list
     for host in hosts:
         ping_host(host, ping_timeout)
-        # result = ping(host, count=1, privileged=False, timeout=2)  
         table.add_row(
-            f"[green]UP" if results[host]['is_alive'] else "[red]DOWN", 
-            f"{host}", 
+            f"[green]UP" if results[host]["is_alive"] else "[red]DOWN",
+            f"{host}",
             # history = ()
             f"{''.join(results[host]['history'][-10:])}",
             # result = ''.join(str(item) for item in list_of_integers)
@@ -62,30 +81,21 @@ def generate_table(ping_timeout) -> Table:
         )
     return table
 
+
 def main(args):
-  with Live(generate_table(args.timeout), refresh_per_second=2) as live:
-    while(True):
-      time.sleep(args.interval)
-      live.update(generate_table(args.timeout))
+    with Live(generate_table(args.timeout), refresh_per_second=2) as live:
+        while True:
+            time.sleep(args.interval)
+            live.update(generate_table(args.timeout))
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-i', '--interval', type=int, default=1, help='ping interval', metavar='i')
-  parser.add_argument('-t', '--timeout', type=int, default=2, help='ping timeout', metavar='i')
-  args = parser.parse_args()
-  main(args)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--interval", type=int, default=1, help="ping interval", metavar="i"
+    )
+    parser.add_argument(
+        "-t", "--timeout", type=int, default=2, help="ping timeout", metavar="i"
+    )
+    args = parser.parse_args()
+    main(args)
